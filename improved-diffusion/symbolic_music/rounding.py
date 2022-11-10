3bcfd15a7672c544784f96f312405bddf6227dc5
Hao Sun <nagisa940216@gmail.com>
2022-11-10 16:29:43 +0800

    Update rounding.py

--- improved-diffusion/symbolic_music/rounding.py
+++ improved-diffusion/symbolic_music/rounding.py
@@ -17,7 +17,10 @@ def get_tokenizer_cls(data_args):
 def load_embedding_model(data_args):
     tokenizer = get_tokenizer_cls(data_args)(sos_eos_tokens=True, mask=False)
     model = torch.nn.Embedding(len(tokenizer.vocab), data_args.in_channel)
-    path_to_load = os.path.join(os.path.split(data_args.model_path)[0], "random_emb.torch")
+    if hasattr(data_args, 'model_path'):
+        path_to_load = os.path.join(os.path.split(data_args.model_path)[0], "random_emb.torch")
+    else:
+        path_to_load = f'{data_args.checkpoint_path}/random_emb.torch'
     model.load_state_dict(torch.load(path_to_load))
     return model
 
