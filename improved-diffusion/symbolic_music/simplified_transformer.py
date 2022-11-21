6ddac3d2a1c9e50e1e843a537305e434cde2b630
Hao Sun <nagisa940216@gmail.com>
2022-11-21 21:18:57 +0800

    add notes

--- improved-diffusion/symbolic_music/simplified_transformer.py
+++ improved-diffusion/symbolic_music/simplified_transformer.py
@@ -67,11 +67,13 @@ class TransformerNetModel2(nn.Module):
         )
         print(config)
         # 下述BertLayer * 12
+        # 768 ->
         # attention(SelfAttention + output(dense + LayerNorm + drop)) + 放大层dense + output(dense + LayerNorm + drop)
+        # -> 768
         self.input_transformers = BertEncoder(config)
-
+        # self.position_ids
         self.register_buffer("position_ids", torch.arange(config.max_position_embeddings).expand((1, -1)))
-        # position embedding = 512 * 768
+        # position embedding = 512 -> 768
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
         self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
@@ -84,12 +86,14 @@ class TransformerNetModel2(nn.Module):
         )
 
     def get_embeds(self, input_ids):
+        # shape -> [*shape, in_channels]
         return self.word_embedding(input_ids)
 
     def get_logits(self, hidden_repr):
+        # in_channels (~16) -> vocab_size
         return self.lm_head(hidden_repr)
 
-    def forward(self, x, timesteps, y=None, src_ids=None, src_mask=None):
+    def forward(self, x, timesteps, src_ids=None, src_mask=None):
         """
         Apply the model to an input batch.
 
@@ -98,6 +102,7 @@ class TransformerNetModel2(nn.Module):
         :param y: an [N] Tensor of labels, if class-conditional.
         :return: an [N x C x ...] Tensor of outputs.
         """
+        #  timesteps  (1,2,3,4...)  ->    sine positional embedding    ->     128 -> 512 -> 768
         emb = self.time_embed(timestep_embedding(timesteps, self.model_channels))
 
         if self.conditional_gen:
@@ -108,10 +113,14 @@ class TransformerNetModel2(nn.Module):
             encoder_hidden_states = self.encoder(src_emb).last_hidden_state
             encoder_attention_mask = src_mask.unsqueeze(1).unsqueeze(1)
 
+        # in_channels (16) -> 768(hidden_size) -> 768(hidden_size)
         emb_x = self.input_up_proj(x)
+
         seq_length = x.size(1)
-        position_ids = self.position_ids[:, : seq_length ]
+        position_ids = self.position_ids[:, : seq_length]
         # print(emb_x.shape, emb.shape, self.position_embeddings)
+
+        # (,768)
         emb_inputs = self.position_embeddings(position_ids) + emb_x + emb.unsqueeze(1).expand(-1, seq_length, -1)
         emb_inputs = self.dropout(self.LayerNorm(emb_inputs))
         if self.conditional_gen:
@@ -121,7 +130,9 @@ class TransformerNetModel2(nn.Module):
                                                                 encoder_attention_mask=encoder_attention_mask,
                                                                 ).last_hidden_state
         else:
+            # 768 -> 768
             input_trans_hidden_states = self.input_transformers(emb_inputs).last_hidden_state
+        # (,768) -> (,16)
         h = self.output_down_proj(input_trans_hidden_states)
         h = h.type(x.dtype)
         return h
