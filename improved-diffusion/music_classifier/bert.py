fd5cc61588b89e49162b9f8ce467364e2e9f57f3
Hao Sun <nagisa940216@gmail.com>
2022-11-30 23:52:33 +0800

    add pretrain

--- improved-diffusion/music_classifier/bert.py
+++ improved-diffusion/music_classifier/bert.py
@@ -8,7 +8,7 @@ from datasets import Dataset
 from miditoolkit import MidiFile
 
 from improved_diffusion.script_util import add_dict_to_argparser
-from music_classifier.easy_bert_classifier import BertNetForSequenceClassification
+from music_classifier.easy_bert_classifier import BertNetForSequenceClassification, BertNetForPreTraining
 from symbolic_music.advanced_padding import advanced_remi_bar_block
 from symbolic_music.utils import get_tokenizer
 from transformers import BertConfig, BertModel, AutoModelForSequenceClassification, TrainingArguments, Trainer, \
@@ -37,6 +37,20 @@ def create_dataset(data_args, split='train'):
     return x, y
 
 
+def create_pre_train_model(data_args):
+    config = BertConfig.from_pretrained("bert-base-uncased")
+    tokenizer = get_tokenizer(data_args)
+    config.vocab_size = len(tokenizer.vocab)
+    model = BertNetForPreTraining(config, data_args.input_emb_dim)
+    if torch.cuda.is_available():
+        learned_embeddings = torch.load(data_args.path_learned)['word_embedding.weight']
+    else:
+        learned_embeddings = torch.load(data_args.path_learned, map_location=torch.device('cpu'))['word_embedding.weight']
+    model.bert.embeddings.word_embeddings.weight.data = learned_embeddings.clone()
+    model.bert.embeddings.word_embeddings.weight.requires_grad = False
+    return model
+
+
 def create_model(data_args, num_labels, id2label, label2id):
     config = BertConfig.from_pretrained("bert-base-uncased")
     config.num_labels = num_labels
@@ -53,6 +67,30 @@ def create_model(data_args, num_labels, id2label, label2id):
     model.bert.embeddings.word_embeddings.weight.data = learned_embeddings.clone()
     model.bert.embeddings.word_embeddings.weight.requires_grad = False
     return model
+
+
+def pre_train(data_args, data_train):
+    model = create_pre_train_model(data_args)
+    training_args = TrainingArguments(
+        output_dir=data_args.output_path,
+        # learning_rate=1e-4,
+        per_device_train_batch_size=data_args.batch_size,
+        per_device_eval_batch_size=data_args.batch_size,
+        num_train_epochs=data_args.epoches,
+        weight_decay=0.0,
+        do_train=True,
+        logging_steps=1000,
+        evaluation_strategy=IntervalStrategy('steps'),
+        logging_strategy=IntervalStrategy('steps'),
+        save_steps=1000,
+        seed=102,
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=data_train,
+    )
+    trainer.train()
 
 
 def train(data_args, data_train, data_valid, num_labels, id2label, label2id):
@@ -91,6 +129,7 @@ def create_argparser():
         padding_mode='bar_block',
         epoches=200,
         batch_size=64,
+        task='train',
         path_learned='./diffusion_models/diff_midi_midi_files_REMI_bar_block_rand32_transformer_lr0.0001_0.0_2000_sqrt_Lsimple_h128_s2_d0.1_sd102_xstart_midi/model200000.pt'
     )
     parser = argparse.ArgumentParser()
@@ -116,4 +155,7 @@ def create_data(args):
 
 if __name__ == '__main__':
     args = create_argparser().parse_args()
-    train(args, *create_data(args))
+    if args.task == 'train':
+        train(args, *create_data(args))
+    elif args.task == 'pre-train':
+        pre_train(args, create_data(args)[0])
--- improved-diffusion/music_classifier/easy_bert_classifier.py
+++ improved-diffusion/music_classifier/easy_bert_classifier.py
@@ -5,6 +5,178 @@ from torch import nn
 from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union
 from torch.nn import CrossEntropyLoss, MSELoss, BCEWithLogitsLoss
 
+from transformers.models.bert.modeling_bert import BertForPreTrainingOutput, BertPreTrainingHeads
+
+
+class BertNetForPreTraining(BertPreTrainedModel):
+    def __init__(self, config, input_emb_dim):
+        super().__init__(config)
+
+        self.bert = BertModel(config)
+        self.bert.embeddings.word_embeddings = nn.Embedding(config.vocab_size, input_emb_dim, )
+        self.up_proj = nn.Sequential(nn.Linear(input_emb_dim, input_emb_dim * 4), nn.Tanh(),
+                                     nn.Linear(input_emb_dim * 4, config.hidden_size))
+        self.cls = BertPreTrainingHeads(config)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def get_output_embeddings(self):
+        return self.cls.predictions.decoder
+
+    def set_output_embeddings(self, new_embeddings):
+        self.cls.predictions.decoder = new_embeddings
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        next_sentence_label=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if inputs_embeds is None:
+            inputs_embeds = self.bert.embeddings.word_embeddings(input_ids)  # input_embs
+
+        inputs_embeds = self.up_proj(inputs_embeds)
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        sequence_output, pooled_output = outputs[:2]
+        prediction_scores, seq_relationship_score = self.cls(sequence_output, pooled_output)
+
+        total_loss = None
+        if labels is not None and next_sentence_label is not None:
+            loss_fct = CrossEntropyLoss()
+            masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
+            next_sentence_loss = loss_fct(seq_relationship_score.view(-1, 2), next_sentence_label.view(-1))
+            total_loss = masked_lm_loss + next_sentence_loss
+
+        if not return_dict:
+            output = (prediction_scores, seq_relationship_score) + outputs[2:]
+            return ((total_loss,) + output) if total_loss is not None else output
+
+        return BertForPreTrainingOutput(
+            loss=total_loss,
+            prediction_logits=prediction_scores,
+            seq_relationship_logits=seq_relationship_score,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+class BertNetPretrainedModel(BertPreTrainedModel):
+    def __init__(self, config, input_emb_dim):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+
+        self.bert = BertModel(config)
+        self.bert.embeddings.word_embeddings = nn.Embedding(config.vocab_size, input_emb_dim, )
+
+        self.up_proj = nn.Sequential(nn.Linear(input_emb_dim, input_emb_dim * 4), nn.Tanh(),
+                                     nn.Linear(input_emb_dim * 4, config.hidden_size))
+
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+        self.post_init()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
+            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if inputs_embeds is None:
+            inputs_embeds = self.bert.embeddings.word_embeddings(input_ids)  # input_embs
+
+        inputs_embeds = self.up_proj(inputs_embeds)
+
+        outputs = self.bert(
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+
 
 class BertNetForSequenceClassification(BertPreTrainedModel):
     def __init__(self, config, input_emb_dim):
